e9268e6265d7ca9b3c17865f83a49f0801549be9
Briggs-Py <briggs.mcknight@gmail.com>
2024-05-24 13:33:01 -0400

    Update AlgorithmsChapter9.js

--- AlgorithmsChapter9.js
+++ AlgorithmsChapter9.js
@@ -1,7 +1,7 @@
 //Chapter 9 - Recursion
 
 //Recursive Sigma
-console.log("Sigma:")
+console.log("Recursive Sigma:")
 //Write a recursive function that given a number, returns sum of integers from 1 to that number.
 function rSigma(n) {
   if (n <= 0) {
@@ -12,3 +12,20 @@ function rSigma(n) {
   }
   return n + rSigma(n - 1);
 }
+console.log(rSigma(3));
+
+//Recursive Factorial
+console.log("\nRecursive Factorial:")
+//Write a function that given 'num', return the product of integers from 1 up to that 'num'.
+function rFactorial(n) {
+  if (n <= 0) {
+    return 1;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * rFactorial(n - 1);
+}
+
+console.log(rFactorial(6));
+
