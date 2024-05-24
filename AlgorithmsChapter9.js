3c1eaf28cc975a0cf1c3fd1c50ca90454df35308
Briggs-Py <briggs.mcknight@gmail.com>
2024-05-24 13:26:43 -0400

    Create AlgorithmsChapter9.js

---
+++ AlgorithmsChapter9.js
@@ -0,0 +1,14 @@
+//Chapter 9 - Recursion
+
+//Recursive Sigma
+console.log("Sigma:")
+//Write a recursive function that given a number, returns sum of integers from 1 to that number.
+function rSigma(n) {
+  if (n <= 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n + rSigma(n - 1);
+}
